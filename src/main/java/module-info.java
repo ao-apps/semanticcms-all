8a5ac66c8eb0204074e50f90318fc78e2e4ce24c
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:51:33 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-all - Convenience POM to include most SemanticCMS features, not including documentation, AutoGit, or theme.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,13 +21,13 @@
  * along with semanticcms-all.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.semanticcms.all {
-	// Direct
-	requires transitive com.semanticcms.changelog.taglib; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-changelog-taglib</artifactId>
-	requires transitive com.semanticcms.core.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-all</artifactId>
-	requires transitive com.semanticcms.dia.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-all</artifactId>
-	requires transitive com.semanticcms.file.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-all</artifactId>
-	requires transitive com.semanticcms.news.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-all</artifactId>
-	requires transitive com.semanticcms.openfile.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-openfile-all</artifactId>
-	requires transitive com.semanticcms.section.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-all</artifactId>
-	requires transitive com.semanticcms.tagreference; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-tag-reference</artifactId>
+  // Direct
+  requires transitive com.semanticcms.changelog.taglib; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-changelog-taglib</artifactId>
+  requires transitive com.semanticcms.core.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-all</artifactId>
+  requires transitive com.semanticcms.dia.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-all</artifactId>
+  requires transitive com.semanticcms.file.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-all</artifactId>
+  requires transitive com.semanticcms.news.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-all</artifactId>
+  requires transitive com.semanticcms.openfile.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-openfile-all</artifactId>
+  requires transitive com.semanticcms.section.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-all</artifactId>
+  requires transitive com.semanticcms.tagreference; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-tag-reference</artifactId>
 }
